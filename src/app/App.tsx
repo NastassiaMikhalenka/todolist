7e2a77b19ac5305871ed24a7312a8cfc55ab58f3
nastassia <anastasiyamihalenko@gmail.com>
2022-02-24 21:05:44 +0300

    connected formik, created loginComponent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import ButtonAppBar from "../components/AppBar";
 import {Container} from "@mui/material";
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
+import {Login} from "../features/Login/Login";
+import { Routes, Route } from "react-router-dom";
 
 type PropsType = {
     demo?: boolean
@@ -14,7 +16,11 @@ function App({demo = false}: PropsType) {
         <div className="App">
             <ButtonAppBar/>
             <Container fixed>
-              <TodolistsList demo={demo}/>
+                <Routes>
+                    <Route path='/' element={<TodolistsList demo={demo}/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path="*" element={<h1>404: PAGE NOT FOUND</h1>}/>
+                </Routes>
             </Container>
         </div>
     );
